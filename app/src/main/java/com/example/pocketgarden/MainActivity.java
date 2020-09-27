1a9e984dd7dc1cfcc8c1bf9b8a0fba6068519573
Pratiik <kaushikp@vcu.edu>
2020-09-26 22:14:37 -0400

    added journal editor, needs fixing still

--- app/src/main/java/com/example/pocketgarden/MainActivity.java
+++ app/src/main/java/com/example/pocketgarden/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.pocketgarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
+
 
 
     @Override
@@ -12,10 +20,15 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
 
-
-
+    /**
+     * Opens journal notes editor
+     * @param view
+     */
+    public void showJournalClick(View view){
+        Intent intent = new Intent(getApplicationContext(), ShowJournals.class);
+        startActivity(intent);
+    }
 
 }
