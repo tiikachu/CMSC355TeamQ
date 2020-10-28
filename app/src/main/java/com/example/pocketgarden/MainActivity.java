f753c81571025016d851659141a2c8d64962daec
Yashna Nainani <nainaniyr@vcu.edu>
2020-10-28 03:59:52 -0400

    nav bar

--- app/src/main/java/com/example/pocketgarden/MainActivity.java
+++ app/src/main/java/com/example/pocketgarden/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.pocketgarden;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewNavBar();
+    }
+
+    protected void viewNavBar(){
+
+        var navController = findNavController(R.id.fragNavHost);
+        bottomNavView.setupWithNavController(navController);
     }
 
 
