0cc634b289fc36e8ed817e2d2285b44131ae0985
Yashna Nainani <nainaniyr@vcu.edu>
2020-11-05 03:37:19 -0500

    update nav bar

--- app/src/main/java/com/example/pocketgarden/MainActivity.java
+++ app/src/main/java/com/example/pocketgarden/MainActivity.java
@@ -12,10 +12,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        NavController navController = Navigation.findNavController(this,  R.id.fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
 
 
     }
