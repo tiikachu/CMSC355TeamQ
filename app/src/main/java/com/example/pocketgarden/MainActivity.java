7b6c54fbc9c9cb310b9900bf5217b7ff293dc708
BesterdatLaptop\Public_void(JD) <depazgonzalej@vcu.edu>
2020-11-13 23:50:41 -0500

    Color update

--- app/src/main/java/com/example/pocketgarden/MainActivity.java
+++ app/src/main/java/com/example/pocketgarden/MainActivity.java
@@ -45,15 +45,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             // connect my plants card to whatever class
             case R.id.My_Plants:
-                intent = new Intent (this, plant.class );
+                intent = new Intent (this, plant.class ); break;
             case R.id.Basics:
-                intent = new Intent (this, Basics.class);
+                intent = new Intent (this, Basics.class); break;
             case R.id.Weather:
-                intent = new Intent (this, Weather.class);
+                intent = new Intent (this, Weather.class); break;
             case R.id.PlantCare:
-                intent = new Intent (this, Notifications.class);
+                intent = new Intent (this, Notifications.class); break;
             case R.id.Library:
-                intent = new Intent (this, plant_listing.class);
+                intent = new Intent (this, plant_listing.class); break;
             case R.id.Journal:
                 intent = new Intent (this, ShowJournals.class);
                 break;
