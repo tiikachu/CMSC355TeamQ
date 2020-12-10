2b67568c7a1346e5ffb91da28f7783e579b5d188
Christine Angeles <angelescc@vcu.edu>
2020-12-09 23:28:39 -0500

    Created CurrentWeatherCallback interface. Created CurrentWeatherService.java that fetches data from API and returns it asynchronously to CurrentWeather object. Created CurrentWeatherUtils to return icon based on weather condition ID from API. Update Weather.java with toggleProgress and CurrentWeatherCallback object.

--- app/src/main/java/com/example/pocketgarden/weather/CurrentWeather.java
+++ app/src/main/java/com/example/pocketgarden/weather/CurrentWeather.java
@@ -1,23 +1,25 @@
+/**
+ * CurrentWeather class with location, conditon ID for figuring out icon that matches with weather condition,
+ * weather condition, and temperature in Kelvin
+ */
 package com.example.pocketgarden.weather;
 
 public class CurrentWeather {
     final String location;
     final int conditionId;
     final String weatherCondition;
-    final double tempKelvin;
+    final double tempKel;
 
-    public CurrentWeather (final String location,
-            final int conditionId,
-            final String weatherCondition,
-            final double tempKelvin) {
+    public CurrentWeather (final String location, final int conditionId, final String weatherCondition, final double tempKel) {
         this.location = location;
         this.conditionId = conditionId;
         this.weatherCondition = weatherCondition;
-        this.tempKelvin = tempKelvin;
+        this.tempKel = tempKel;
     }
 
+    // API data value for temperature is Kelvin so convert to Fahrenheit
     public int convertToFahrenheit () {
-        return (int) (tempKelvin * 9/5 - 459.67);
+        return (int) (tempKel * 9/5 - 459.67);
     }
 
 
--- app/src/main/java/com/example/pocketgarden/weather/Weather.java
+++ app/src/main/java/com/example/pocketgarden/weather/Weather.java
@@ -1,31 +1,45 @@
 package com.example.pocketgarden.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.pocketgarden.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import com.example.pocketgarden.R;
+
 public class Weather extends AppCompatActivity {
+
     private TextView temperature, location, weatherCondition;
+    private ProgressBar weatherProgressBar;
+    private View weatherContainer;
     private ImageView weatherConditionIcon;
     private EditText locationField;
     private FloatingActionButton floatingActionButton;
+
+    private String currentLocation = "Richmond";
     private int textCount = 0;
+
+    private CurrentWeatherService currentWeatherService;
+    private boolean gettingWeather = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        weatherContainer = findViewById(R.id.weatherContainer);
+        weatherProgressBar = findViewById(R.id.weatherProgressBar);
         temperature = findViewById(R.id.temperature);
         location = findViewById(R.id.location);
         weatherCondition = findViewById(R.id.weatherCondition);
@@ -33,12 +47,13 @@ public class Weather extends AppCompatActivity {
         locationField = findViewById(R.id.locationField);
         floatingActionButton = findViewById(R.id.fab);
 
+        currentWeatherService = new CurrentWeatherService(this);
+
         locationField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-            }
-
+            // When text is changed, refresh icon changes to search icon
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 count = s.toString().trim().length();
@@ -52,9 +67,7 @@ public class Weather extends AppCompatActivity {
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
-
-            }
+            public void afterTextChanged(Editable s) {}
         });
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -69,15 +82,61 @@ public class Weather extends AppCompatActivity {
                 }
             }
         });
+        searchForWeather(currentLocation);
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        currentWeatherService.cancel();
     }
 
     private void refreshWeather () {
-        Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+        if (gettingWeather) {
+            return;
+        }
+        searchForWeather(currentLocation);
     }
 
-    private void searchForWeather (final String location) {
-        Toast.makeText(this, "Search for: " + location, Toast.LENGTH_SHORT).show();
+    private void searchForWeather (@NonNull final String location) {
+        toggleProgress(true);
+        gettingWeather = true;
+        currentWeatherService.getCurrentWeather(location, currentWeatherCallback);
     }
+
+    private void toggleProgress (final boolean showProgress) {
+        if (showProgress) {
+            weatherContainer.setVisibility(View.GONE);
+            weatherProgressBar.setVisibility(View.VISIBLE);
+        }
+        else {
+            weatherContainer.setVisibility(View.VISIBLE);
+            weatherProgressBar.setVisibility(View.GONE);
+        }
+    }
+
+    private final CurrentWeatherCallback currentWeatherCallback = new CurrentWeatherCallback() {
+
+        // Take properties of CurrentWeather and populate with UI
+        @Override
+        public void onCurrentWeather(@NonNull CurrentWeather currentWeather) {
+            currentLocation = currentWeather.location;
+            temperature.setText(String.valueOf(currentWeather.convertToFahrenheit()));
+            location.setText(currentWeather.location);
+            weatherCondition.setText(currentWeather.weatherCondition);
+            weatherConditionIcon.setImageResource(CurrentWeatherUtils.getWeatherIconResId
+                    (currentWeather.conditionId));
+            toggleProgress(false);
+            gettingWeather = false;
+        }
+
+        @Override
+        public void onError(@Nullable Exception exception) {
+            toggleProgress(false);
+            gettingWeather = false;
+            Toast.makeText(Weather.this, "There was an error getting weather data. Try again", Toast.LENGTH_SHORT).show();
+        }
+    };
 
 
 }
